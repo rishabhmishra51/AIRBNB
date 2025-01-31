e97d0256a7f6250e29bc97e45f00c06b31ab0f53
rishabhmishra51 <rishabhmishra7570@gmail.com>
2025-02-01 00:09:57 +0530

    changes

--- middleware.js
+++ middleware.js
@@ -1,5 +1,5 @@
 const Listing = require("./models/listing");
-const Review = require("../Airbnb/models/review");
+const Review = require("./models/review");
 module.exports.isLoggedIn = (req,res,next)=>{ 
      if(!req.isAuthenticated()){
           req.session.redirectUrl = req.originalUrl;
